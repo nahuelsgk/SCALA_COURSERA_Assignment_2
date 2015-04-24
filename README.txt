7ec96cd2ce05f3d54a36b1b09209ba472b7279cc
Nahuel Velazco <nahuel@Rodio@fapac.org>
2015-04-25 00:23:42 +0200

    First exercise

---
+++ README.txt
@@ -0,0 +1,15 @@
+Entorno:
+> cd /home/nahuel/Desktop/Projectos/COURSERA/ReactiveProgramming/Assignment_Week2/calculator
+> /home/nahuel/git/sbt/bin/sbt
+
+[] 1a Tarea
+Implementar la la funcion 
+src/scala/calculator/TweetLength.scala:def tweetRemainingCharsCount
+f: Signal[String] => Signal[Int]
+
+La funcion tiene un Signal como parametro.
+Los lanzar un eventos es lanzar una señal. Las operaciones fundamentales son.
+1) La señal tiene un valor concreto.
+2) Definir señales en funcion de otras señales.
+
+
---
+++ src/main/scala/calculator/TweetLength.scala
@@ -0,0 +1,48 @@
+package calculator
+
+object TweetLength {
+  final val MaxTweetLength = 140
+
+   
+  def tweetRemainingCharsCount(tweetText: Signal[String]): Signal[Int] = {
+    println("Calculando remaining chars")
+    Signal 
+        {
+          val string = tweetText()
+          println(string)
+          MaxTweetLength-tweetLength(string)
+        }
+  }
+
+  def colorForRemainingCharsCount(remainingCharsCount: Signal[Int]): Signal[String] = {
+    val remaining = remainingCharsCount()
+    Signal{
+      println(remaining)  //Poque siempre es el mismo. Porque no se acttualiza
+      println(remainingCharsCount())
+      remainingCharsCount() match {
+        case good if 14  < good                => "green"
+        case warn if 0   <= warn && warn <= 14 => "orange"
+        case bad  if bad < 0                   => "red"
+      }
+    }
+    //Signal("red")
+  }
+
+  /** Computes the length of a tweet, given its text string.
+   *  This is not equivalent to text.length, as tweet lengths count the number
+   *  of Unicode *code points* in the string.
+   *  Note that this is still a simplified view of the reality. Full details
+   *  can be found at
+   *  https://dev.twitter.com/overview/api/counting-characters
+   */
+  private def tweetLength(text: String): Int = {
+    /* This should be simply text.codePointCount(0, text.length), but it
+     * is not implemented in Scala.js 0.6.2.
+     */
+    if (text.isEmpty) 0
+    else {
+      text.length - text.init.zip(text.tail).count(
+          (Character.isSurrogatePair _).tupled)
+    }
+  }
+}
